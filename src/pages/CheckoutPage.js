0ca84c5b6276d77f328771afab8ff46d3e41adb9
Zaidshaikh2811 <zaidshaikhhulk@gmail.com>
2023-12-27 23:36:43 +0530

    ten

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,15 +1,39 @@
 import React from "react";
 import styled from "styled-components";
 import { PageHero, StripeCheckout } from "../components";
+import { useCartContext } from "../context/cart_context";
 // extra imports
-
+import { Link } from "react-router-dom";
 const CheckoutPage = () => {
+  const { cart } = useCartContext();
   return (
     <main>
       <PageHero title="checkout"></PageHero>
-      <Wrapper className="page"></Wrapper>
+      <Wrapper className="page">
+        {
+          // if the cart is empty show a message saying so. Otherwise display the checkout form
+          cart.length < 1 ? (
+            <div className="empty">
+              <h2>Your Cart is Empty</h2>
+              <Link to="/products" className="btn">
+                Fill It
+              </Link>
+            </div>
+          ) : (
+            <StripeCheckout></StripeCheckout>
+          )
+        }
+      </Wrapper>
     </main>
   );
 };
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  justify-content: center;
+  .empty {
+    text-align: center;
+  }
+`;
 export default CheckoutPage;
